094fbc12ccc2babec9b159abcb1a44b0e7a4f18f
Nitish Poonia <npoonia39@gmail.com>
2024-01-08 07:59:40 +0530

    Final Project

--- Xylophone/ViewController.swift
+++ Xylophone/ViewController.swift
@@ -1,21 +1,29 @@
-//
-//  ViewController.swift
-//  Xylophone
-//
-//  Created by Angela Yu on 28/06/2019.
-//  Copyright © 2019 The App Brewery. All rights reserved.
-//
-
 import UIKit
+import AVFoundation
 
 class ViewController: UIViewController {
+    
+    var player: AVAudioPlayer!
 
     override func viewDidLoad() {
         super.viewDidLoad()
     }
 
-
+    @IBAction func keyPressed(_ sender: UIButton) {
+        let buttonTitle = sender.currentTitle
+        playSound(buttonTitle)
+        
+    }
     
-
+    func playSound(_ string: String?) {
+        guard let title = string else {
+                    print("Button title is nill")
+                    return
+                }
+        
+        let url = Bundle.main.url(forResource: title, withExtension: "wav")
+        player = try! AVAudioPlayer(contentsOf: url!)
+        player.play()
+                
+    }
 }
-
